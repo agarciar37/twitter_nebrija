3e6deac3e3347521276cb4370595ed76681d8e81
agarciar37 <92687879+agarciar37@users.noreply.github.com>
2026-05-01 19:24:43 +0200

    Refactor UserPage component and remove unused code

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,30 +1,18 @@
 'use client'
 
-
 import { getUser } from "@/api/enlaces";
 import { Usuario } from "@/types";
-import { useEffect, useEffectEvent, useState } from "react";
+import { useEffect, useState } from "react";
 import { CardUser } from "../components/Carduser";
 
+const UserPage = () => {
+  const [user, setUser] = useState<Usuario>();
 
+  useEffect(() => {
+    getUser().then(setUser);
+  }, []);
 
+  return <div className="perfil-container">{user && <CardUser user={user} />}</div>;
+};
 
-const userPage =() => {
-
-    const [user,setUser] = useState<Usuario>()
-
-    useEffect(()=>{getUser().then(setUser);},[])
-    console.log("esto es una prueba")
-
-    console.log(user)
-
-return(
-    <div className="perfil-container">
-        {user && <CardUser user={user}/>}
-    </div>
-)
-
-}
-
-
-export default userPage;
+export default UserPage;
