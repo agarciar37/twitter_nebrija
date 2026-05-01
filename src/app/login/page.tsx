afb59c0f983e2b931b241f0dbeb75c45eb355754
agarciar37 <92687879+agarciar37@users.noreply.github.com>
2026-05-01 19:24:18 +0200

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,56 +1,31 @@
 'use client'
 
-
-import Image from "next/image";
-import styles from "./page.module.css";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Login } from "../components/login";
 import { Register } from "../components/register";
 
- const   login = () => {
+const LoginPage = () => {
+  const [isLogin, setIsLogin] = useState(true);
 
-  const router=useRouter()
-  
+  return (
+    <div className="auth-container">
+      {isLogin ? (
+        <div className="auth-box">
+          <button className="auth-switch-btn" onClick={() => setIsLogin(false)}>
+            Register
+          </button>
+          <Login />
+        </div>
+      ) : (
+        <div className="auth-box">
+          <button className="auth-switch-btn" onClick={() => setIsLogin(true)}>
+            Login
+          </button>
+          <Register />
+        </div>
+      )}
+    </div>
+  );
+};
 
-  const [mostrar,setMostrar] = useState<string>()
-  const [login,setLogin] = useState<boolean>(true)
-  const [register,setRegister] = useState<boolean>(false)
-  
-
-  
- return (
-  <div className="auth-container">
-
-    {login &&
-      <div className="auth-box"> 
-        <button 
-          className="auth-switch-btn"
-          onClick={() => {setRegister(true); setLogin(false)}}
-        >
-          Register
-        </button>
-
-        <Login/>
-      </div>
-    }
-
-    {register &&
-      <div className="auth-box"> 
-        <button 
-          className="auth-switch-btn"
-          onClick={() => {setRegister(false); setLogin(true)}}
-        >
-          Login
-        </button>
-
-        <Register/>
-      </div>
-    }
-
-  </div>
-);
-}
-
-
-export default login;
+export default LoginPage;
