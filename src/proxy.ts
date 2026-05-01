ee4a0519e766a6b7b68b08a32d7f9993245d73ab
agarciar37 <92687879+agarciar37@users.noreply.github.com>
2026-05-01 19:22:16 +0200

    Refactor proxy function and update protected routes

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,25 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export const proxy =(request: NextRequest) => {
-  console.log("URL:", request.url);
-
-  // 🔥 leer token de cookies
+export const proxy = (request: NextRequest) => {
   const token = request.cookies.get("token");
 
-  // ❌ si no hay token → redirigir a login
   if (!token) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
-  // ✅ si hay token → dejar pasar
   return NextResponse.next();
-}
+};
 
-// 🔥 rutas protegidas
 export const config = {
-  matcher: [
-    "/", 
-    "/perfil/:path*", 
-    "/post/:path*"
-  ],
+  matcher: ["/", "/user/:path*", "/perfiles/:path*", "/post/:path*", "/:id"],
 };
