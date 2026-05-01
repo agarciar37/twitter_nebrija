67181196d594af837035633a22cfd831f82a0756
agarciar37 <92687879+agarciar37@users.noreply.github.com>
2026-05-01 19:23:45 +0200

    Refactor CardUser component for pagination

--- src/app/components/Carduser.tsx
+++ src/app/components/Carduser.tsx
@@ -1,29 +1,32 @@
-import {  Usuario } from "@/types";
+import { Usuario } from "@/types";
 import { CardHome } from "./CardHome";
+import { useMemo, useState } from "react";
+import { Paginador } from "./Paginador";
 
+const PAGE_SIZE = 10;
 
+export const CardUser = ({ user }: { user: Usuario }) => {
+  const [page, setPage] = useState(1);
+  const totalPaginas = Math.max(1, Math.ceil(user.posts.length / PAGE_SIZE));
 
-export const CardUser = ({user}:{user:Usuario}) =>{
+  const postsPaginados = useMemo(() => {
+    const start = (page - 1) * PAGE_SIZE;
+    return user.posts.slice(start, start + PAGE_SIZE);
+  }, [page, user.posts]);
 
-    return (
-  <div className="perfil-box">
+  return (
+    <div className="perfil-box">
+      <div className="perfil-header">
+        <h1 className="perfil-username">{user.user.username}</h1>
+      </div>
 
-    <div className="perfil-header">
-      <h1 className="perfil-username">{user.user.username}</h1>
+      <div className="perfil-posts">
+        {postsPaginados.map((e) => (
+          <CardHome key={e._id} post={e} />
+        ))}
+      </div>
+
+      <Paginador page={page} setPage={setPage} totalpaginas={totalPaginas} />
     </div>
-
-    <div className="perfil-posts">
-      {user.posts.map((e)=> (
-        <CardHome key={e._id} post={e}/>
-      ))}
-    </div>
-
-  </div>
-)
-    
-
-
-    
-
-
-}
+  );
+};
